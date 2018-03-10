788f52b425f39407ee05903f33f3182554110340
calumwhite <34377333+calumwhite@users.noreply.github.com>
2018-03-10 17:08:49 +0000

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,8 +1,8 @@
 import random
+import time
 
 def translate(starttime,iD,la,lo):
     #startime is translation between time of commands and unix time
-
     with open("pattern.json","r") as pattern:
 
            sequence = json.loads(pattern.read())
@@ -10,19 +10,19 @@ def translate(starttime,iD,la,lo):
     buffer = []
 
     for things in sequence:
-        params = things.commands.parameters
-        if (random.random() < things.commands.probability):
-            if correctLocation(params, la, lo) and afterStartTime(things, starttime) and correctID(params, iD):
-                buffer.append( ( things.time+starttime, things.commands.command ) )
+        params = things["commands"]["parameters"]
+        if random.random() < things["commands"]["probability"]:
+            if correctLocation(params, la, lo) and ontime(things, starttime) and correctID(params, iD):
+                buffer.append( ( things["time"]+starttime, things["commands"]["command"]) )
 
 
     return buffer
 
 def correctLocation(params, la, lo):
-    return (params.la[0] <= la) and (params.la[1] > la) and (params.lo[0] <= lo) and (params.lo[1] > lo)
+    return (params["la"][0] <= la) and (params["la"][1] > la) and (params["lo"][0] <= lo) and (params["lo"][1] > lo)
 
-def afterStartTime(things, starttime):
-    return (things.time > starttime)
+def ontime(things, starttime):
+    return (things["time"] + starttime >= time.time())
 
 def correctID(params, iD):
-    return (params.iD == iD)
+    return (params["iD"] == iD)
