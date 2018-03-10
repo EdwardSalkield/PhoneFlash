bbf5acb166008f5454cc220294cc6d5beb034166
ggilligan12 <ggilligan12@gmail.com>
2018-03-10 16:27:59 +0000

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,7 +1,28 @@
-translate(starttime,id,la,lo):
-    #startime is translation betweenn time of commands and unix time
+import random
+
+def translate(starttime,iD,la,lo):
+    #startime is translation between time of commands and unix time
+
     with open("pattern.json","r") as pattern:
-           sequence=json.loads(pattern.read())
+
+           sequence = json.loads(pattern.read())
+
+    buffer = []
+
+    for things in sequence:
+        params = things.commands.parameters
+        if (random.random() < things.commands.probability):
+            if correctLocation(params, la, lo) and afterStartTime(things, starttime) and correctID(params, iD):
+                buffer.append( ( things.getTime(), things.getToggle() ) )
 
 
     return buffer
+
+def correctLocation(params, la, lo):
+    return (params.la[0] <= la) and (params.la[1] > la) and (params.lo[0] <= lo) and (params.lo[1] > lo)
+
+def afterStartTime(things, starttime):
+    return (things.time > starttime)
+
+def correctID(params, iD):
+    return (params.iD == iD)
