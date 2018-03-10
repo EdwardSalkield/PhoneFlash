25bd6fdbf3f3ce011a41926e882393c695f8f4d2
TestServer <ec2-user@ip-172-31-10-190.eu-west-2.compute.internal>
2018-03-10 20:15:33 +0000

    Updated build script

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-cd client
-npm run build
-echo Successful build!
-cp -r build /data/build
-echo Done!
+(cd client;
+echo Building client;
+npm run build;
+cp -r build /data/build;
+echo Successful client build!)
+
+(cd server;
+echo Building server;
+cp -r * . /data/server
+)
