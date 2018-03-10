cdf256bfd40429276939fc982bbb2c546fb82dec
Shaz <lightbulbmoment22617@gmail.com>
2018-03-10 20:00:08 +0000

    Updating flashlight on client web app

--- client/src/App.js
+++ client/src/App.js
@@ -44,62 +44,65 @@ class App extends Component {
   }
 
   toggleTorch () {
-    navigator.mediaDevices.getUserMedia({
-      video: {
-        facingMode: 'environment',
-      }
-    })
-    .then((stream) => {
-      const video = document.querySelector('video');
-      //video.srcObject = stream;
-      if (video.mozSrcObject !== undefined) { // FF18a
-              video.mozSrcObject = stream;
-            } else if (video.srcObject !== undefined) {
-              video.srcObject = stream;
-            } else { // FF16a, 17a
-              video.src = stream;
-            }
 
-    })
-    .catch(err => console.error('getUserMedia() failed: ', err));
-    // //Test browser support
-    // const SUPPORTS_MEDIA_DEVICES = 'mediaDevices' in navigator;
+    // window.plugins.flashlight.available(function(isAvailable) {
+    //   if (isAvailable) {
     //
-    // if (SUPPORTS_MEDIA_DEVICES) {
-    //   //Get the environment camera (usually the second one)
-    //   navigator.mediaDevices.enumerateDevices().then(devices => {
-    //     const cameras = devices.filter((device) => device.kind === 'videoinput');
-    //     if (cameras.length === 0) {
-    //       throw 'No camera found on this device.';
-    //     }
-    //     const camera = cameras[cameras.length - 1];
-    //     // Create stream and get video track
-    //     navigator.mediaDevices.getUserMedia({
-    //       video: {
-    //         deviceId: camera.deviceId,
-    //         facingMode: ['user', 'environment'],
-    //         height: {ideal: 1080},
-    //         width: {ideal: 1920}
-    //       }
-    //     }).then(stream => {
-    //       const track = stream.getVideoTracks()[0];
+    //     // switch on
+    //     window.plugins.flashlight.switchOn(
+    //       function() {}, // optional success callback
+    //       function() {}, // optional error callback
+    //       {intensity: 0.3} // optional as well
+    //     );
     //
-    //       //Create image capture object and get camera capabilities
-    //       const imageCapture = new ImageCapture(track)
-    //       const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
-    //         //todo: check if camera has a torch
-    //         //let there be light!
-    //         const btn = document.querySelector('.switch');
-    //         btn.addEventListener('click', function(){
-    //           track.applyConstraints({
-    //             advanced: [{torch: true}]
-    //           });
-    //         });
-    //       });
-    //     });
-    //   });
-    //   //The light will be on as long the track exists
-    // }
+    //     // switch off after 3 seconds
+    //     setTimeout(function() {
+    //       window.plugins.flashlight.switchOff(); // success/error callbacks may be passed
+    //     }, 3000);
+    //
+    //   } else {
+    //     alert("Flashlight not available on this device");
+    //   }
+    // });
+
+    //Test browser support
+    const SUPPORTS_MEDIA_DEVICES = 'mediaDevices' in navigator;
+
+    if (SUPPORTS_MEDIA_DEVICES) {
+      //Get the environment camera (usually the second one)
+      navigator.mediaDevices.enumerateDevices().then(devices => {
+        const cameras = devices.filter((device) => device.kind === 'videoinput');
+        if (cameras.length === 0) {
+          throw 'No camera found on this device.';
+        }
+        const camera = cameras[cameras.length - 1];
+        // Create stream and get video track
+        navigator.mediaDevices.getUserMedia({
+          video: {
+            deviceId: camera.deviceId,
+            facingMode: ['user', 'environment'],
+            height: {ideal: 1080},
+            width: {ideal: 1920}
+          }
+        }).then(stream => {
+          const track = stream.getVideoTracks()[0];
+
+          //Create image capture object and get camera capabilities
+          const imageCapture = new ImageCapture(track)
+          const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
+            //todo: check if camera has a torch
+            //let there be light!
+            const btn = document.querySelector('.switch');
+            btn.addEventListener('click', function(){
+              track.applyConstraints({
+                advanced: [{torch: true}]
+              });
+            });
+          });
+        });
+      });
+      //The light will be on as long the track exists
+    }
   }
 
   render () {
