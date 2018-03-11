013df58f4455799a3b7b14499a60dd5a2705e4c2
ggilligan12 <ggilligan12@gmail.com>
2018-03-11 03:30:08 +0000

    Create onBeat.py

---
+++ server/onBeat.py
@@ -0,0 +1,248 @@
+
+def flashOB(bar, commands , beatTime, barTime):
+
+    time = barTime*bar
+    #RHS on off commands
+    commands.append({   "time":time,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'on',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+(beatTime/4),
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'off',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+beatTime,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'on',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+beatTime+(beatTime/4),
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'off',
+                            "probability":1
+                        }
+                    })
+    #LHS on off commands
+    commands.append({   "time":time+2*beatTime,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'on',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+2*beatTime+(beatTime/4),
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'off',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+3*beatTime,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180,180]
+                            },
+                            "command":'on',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+3*beatTime+(beatTime/4),
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'off',
+                            "probability":1
+                        }
+                    })
+
+    return commands
+
+
+
+
+
+def toggleOB(bar, commands , beatTime, barTime):
+
+    time = barTime*bar
+    #RHS on off commands
+    commands.append({   "time":time,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'on',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+beatTime,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'off',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+2*beatTime,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'on',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+3*beatTime,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180,180]
+                            },
+                            "command":'off',
+                            "probability":1
+                        }
+                    })
+
+    return commands
+
+
+def toggleOB2(bar, commands , beatTime, barTime):
+
+    time = barTime*bar
+    #RHS on off commands
+    commands.append({   "time":time,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'on',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+(beatTime/2),
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'off',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+beatTime,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'on',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+beatTime+(beatTime/2),
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'off',
+                            "probability":1
+                        }
+                    })
+    #LHS on off commands
+    commands.append({   "time":time+2*beatTime,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'on',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+2*beatTime+(beatTime/2),
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'off',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+3*beatTime,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180,180]
+                            },
+                            "command":'on',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+3*beatTime+(beatTime/2),
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'off',
+                            "probability":1
+                        }
+                    })
+
+    return commands
