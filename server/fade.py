22bc4ed358f6dc5dd2bc853f34eedbc2d0f20fdb
ggilligan12 <ggilligan12@gmail.com>
2018-03-11 03:31:43 +0000

    Create fade.py

---
+++ server/fade.py
@@ -0,0 +1,53 @@
+#def fadeIn():
+#def fadeOut():
+
+def dark(bar, commands , beatTime, barTime):
+
+    time = barTime*bar
+    #RHS on off commands
+    commands.append({   "time":time,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'off',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+beatTime,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'off',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+2*beatTime,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180, 180]
+                            },
+                            "command":'off',
+                            "probability":1
+                        }
+                    })
+    commands.append({   "time":time+3*beatTime,
+                        "commands":{
+                            "parameters":{
+                                "iD":None,
+                                "la":[-90,90],
+                                "lo":[-180,180]
+                            },
+                            "command":'off',
+                            "probability":1
+                        }
+                    })
+
+    return commands
